db842d3b63fa56d411a262ac2b86191a726b4112
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2013-05-23 23:04:49 +0200

    Initial commit.

---
+++ gameoflife/board.py
@@ -0,0 +1,29 @@
+'''
+Created on 23/05/2013
+
+@author: Antek
+'''
+import unittest
+
+class Board(object):
+    pass
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
