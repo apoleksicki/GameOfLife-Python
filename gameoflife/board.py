546490001f3e8aa37573e80f1fc67c0a48f02cf5
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2013-05-24 00:21:31 +0200

    Implemented the Point class and function for generate neighboring points.

--- gameoflife/board.py
+++ gameoflife/board.py
@@ -5,6 +5,35 @@ Created on 23/05/2013
 '''
 import unittest
 
+def boardParser(toParse):
+    return toParse.split('\n')
+
+def generateNewCell(board, x, y):
+    pass
+
+def generateNeighborIndex(point, board):
+    allNeighbors = []
+    allNeighbors.append(Point(point.x - 1, point.y - 1))
+    allNeighbors.append(Point(point.x - 1, point.y))
+    allNeighbors.append(Point(point.x - 1, point.y + 1))
+    allNeighbors.append(Point(point.x, point.y + 1))
+    allNeighbors.append(Point(point.x, point.y - 1))
+    allNeighbors.append(Point(point.x + 1, point.y - 1))
+    allNeighbors.append(Point(point.x + 1, point.y))
+    allNeighbors.append(Point(point.x + 1, point.y + 1))
+    return [neighbor for neighbor in allNeighbors if neighbor.x >= 0 and neighbor.x < len(board) and neighbor.y >= 0 and neighbor.y < len(board[0])  ]
+    
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        '''
+        Overrides str
+        '''
+        return 'X: %s Y: %s' % (self.x, self.y)
+        
+
 class Board(object):
     pass
 
@@ -24,6 +53,6 @@ class Test(unittest.TestCase):
         pass
 
 
-if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+#if __name__ == "__main__":
+#    #import sys;sys.argv = ['', 'Test.testName']
+#    unittest.main()
