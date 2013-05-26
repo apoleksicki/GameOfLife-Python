f686426d34f677d8074e320e3a89f04ee5603385
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2013-05-26 14:16:21 +0200

    Implemented function for calculating new cell, added a test case for it.

--- gameoflife/board.py
+++ gameoflife/board.py
@@ -13,11 +13,11 @@ def boardParser(toParse):
 
 def getCell(point, board):
     x, y = point
-    return board(x)(y)
+    return board[x][y]
 
 def generateNewCell(point, board):
     cell = getCell(point, board) 
-    aliveNeighbors = reduce(lambda x,y: x + y, map(getCell, generateNeighborIndex(point, board)))
+    aliveNeighbors = reduce(lambda x,y: x + y, map(lambda point: getCell(point, board), generateNeighborIndex(point, board)))
     
     if DEAD == cell:
         if aliveNeighbors == 3:
@@ -26,6 +26,8 @@ def generateNewCell(point, board):
             return DEAD
     elif aliveNeighbors == 2 or aliveNeighbors == 3:
         return ALIVE
+    else:
+        return DEAD
         
     
 
@@ -42,23 +44,46 @@ def generateNeighborIndex(point, board):
     allNeighbors.append((x + 1, y + 1))
     return [(x, y) for x, y in allNeighbors if x >= 0 and x < len(board) and y >= 0 and y < len(board[0])]
     
-class Board(object):
-    pass
+class TestNewBoardGeneration(unittest.TestCase):
 
+    def test_dead_cell_with_three_neigbors_becomes_alive(self):
+        board = [[0, 1, 0],
+                 [0, 1, 0],
+                 [0, 1, 0]]
+        self.assertEqual(ALIVE, generateNewCell((1,0), board))
+        self.assertEqual(ALIVE, generateNewCell((1,2), board))
+        
+    def test_dead_cell_with_two_neigbors_remains_dead(self):
+        board = [[0, 1, 0],
+                 [0, 1, 0],
+                 [0, 1, 0]]
+        self.assertEqual(DEAD, generateNewCell((0,0), board))
+        self.assertEqual(DEAD, generateNewCell((2,0), board))
+        
+    def test_dead_cell_with_more_three_neighbors_remains_dead(self):
+        board = [[0, 1, 0, 0],
+                 [0, 1, 1, 1],
+                 [0, 1, 0, 1]]
+        self.assertEqual(DEAD, generateNewCell((2,2), board))
+        
+    def test_alive_cell_with_two_neigbors_remains_alive(self):
+        board = [[0, 1, 0],
+                 [0, 1, 0],
+                 [0, 1, 0]]
+        self.assertEqual(ALIVE, generateNewCell((1,1), board))
 
-class Test(unittest.TestCase):
-
-
-    def setUp(self):
-        pass
-
-
-    def tearDown(self):
-        pass
-
-
-    def testName(self):
-        pass
+    def test_alive_cell_with_three_neigbors_remains_alive(self):
+        board = [[0, 1, 1],
+                 [0, 1, 1],
+                 [0, 0, 0]]
+        self.assertEqual(ALIVE, generateNewCell((1,1), board))
+        
+    def test_alive_cell_with_four_neigbors_dies(self):
+        board = [[0, 1, 1],
+                 [0, 1, 1],
+                 [0, 1, 0]]
+        self.assertEqual(DEAD, generateNewCell((1,1), board))
+        
 
 
 #if __name__ == "__main__":
