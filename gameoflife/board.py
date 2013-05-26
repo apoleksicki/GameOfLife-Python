3c1e5888ab4dce509b4f9dc4d8df8d536e1bcf15
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2013-05-26 13:07:31 +0200

    Tuples used instead of instances of Point class implemented generation of a new cell.

--- gameoflife/board.py
+++ gameoflife/board.py
@@ -5,35 +5,43 @@ Created on 23/05/2013
 '''
 import unittest
 
+ALIVE = 1
+DEAD = 0
+
 def boardParser(toParse):
     return toParse.split('\n')
 
-def generateNewCell(board, x, y):
-    pass
+def getCell(point, board):
+    x, y = point
+    return board(x)(y)
+
+def generateNewCell(point, board):
+    cell = getCell(point, board) 
+    aliveNeighbors = reduce(lambda x,y: x + y, map(getCell, generateNeighborIndex(point, board)))
+    
+    if DEAD == cell:
+        if aliveNeighbors == 3:
+            return ALIVE
+        else:
+            return DEAD
+    elif aliveNeighbors == 2 or aliveNeighbors == 3:
+        return ALIVE
+        
+    
 
 def generateNeighborIndex(point, board):
+    x, y = point
     allNeighbors = []
-    allNeighbors.append(Point(point.x - 1, point.y - 1))
-    allNeighbors.append(Point(point.x - 1, point.y))
-    allNeighbors.append(Point(point.x - 1, point.y + 1))
-    allNeighbors.append(Point(point.x, point.y + 1))
-    allNeighbors.append(Point(point.x, point.y - 1))
-    allNeighbors.append(Point(point.x + 1, point.y - 1))
-    allNeighbors.append(Point(point.x + 1, point.y))
-    allNeighbors.append(Point(point.x + 1, point.y + 1))
-    return [neighbor for neighbor in allNeighbors if neighbor.x >= 0 and neighbor.x < len(board) and neighbor.y >= 0 and neighbor.y < len(board[0])  ]
+    allNeighbors.append((x - 1, y - 1))
+    allNeighbors.append((x - 1, y))
+    allNeighbors.append((x - 1, y + 1))
+    allNeighbors.append((x, y + 1))
+    allNeighbors.append((x, y - 1))
+    allNeighbors.append((x + 1, y - 1))
+    allNeighbors.append((x + 1, y))
+    allNeighbors.append((x + 1, y + 1))
+    return [(x, y) for x, y in allNeighbors if x >= 0 and x < len(board) and y >= 0 and y < len(board[0])]
     
-class Point(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def __repr__(self):
-        '''
-        Overrides str
-        '''
-        return 'X: %s Y: %s' % (self.x, self.y)
-        
-
 class Board(object):
     pass
 
