ec58368669466b617be219be09ab76cef66ee839
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2013-05-26 20:54:12 +0200

    All functions put into a class Board.

--- gameoflife/board.py
+++ gameoflife/board.py
@@ -5,100 +5,107 @@ Created on 23/05/2013
 '''
 import unittest
 
-ALIVE = 1
-DEAD = 0
 
 def boardParser(toParse):
     return toParse.split('\n')
 
-def getCell(point, board):
-    x, y = point
-    return board[x][y]
 
-def generateNewCell(point, board):
-    cell = getCell(point, board) 
-    aliveNeighbors = reduce(lambda x,y: x + y, map(lambda point: getCell(point, board), generateNeighborIndex(point, board)))
+class Board(object):
+    ALIVE = 1
+    DEAD = 0
     
-    if DEAD == cell:
-        if aliveNeighbors == 3:
-            return ALIVE
+    def __init__(self, board):
+        self.board = board
+    
+    def _getCell(self, point):
+        x, y = point
+        return self.board[x][y]
+    
+    def _generateNewCell(self, point, board):
+        cell = self._getCell(point) 
+        aliveNeighbors = reduce(lambda x,y: x + y, map(lambda point: self._getCell(point), self._generateNeighborIndex(point)))
+        
+        if Board.DEAD == cell:
+            if aliveNeighbors == 3:
+                return Board.ALIVE
+            else:
+                return Board.DEAD
+        elif aliveNeighbors == 2 or aliveNeighbors == 3:
+            return Board.ALIVE
         else:
-            return DEAD
-    elif aliveNeighbors == 2 or aliveNeighbors == 3:
-        return ALIVE
-    else:
-        return DEAD
+            return Board.DEAD
+            
         
     
-
-def generateNeighborIndex(point, board):
-    x, y = point
-    allNeighbors = []
-    allNeighbors.append((x - 1, y - 1))
-    allNeighbors.append((x - 1, y))
-    allNeighbors.append((x - 1, y + 1))
-    allNeighbors.append((x, y + 1))
-    allNeighbors.append((x, y - 1))
-    allNeighbors.append((x + 1, y - 1))
-    allNeighbors.append((x + 1, y))
-    allNeighbors.append((x + 1, y + 1))
-    return [(x, y) for x, y in allNeighbors if x >= 0 and x < len(board) and y >= 0 and y < len(board[0])]
-
-def generateNewBoard(board):
-    result = []
-    for x in range(len(board)):
-        result.append([])
-        for y in range(len(board[0])):
-            result[x].append(generateNewCell((x, y), board))
-    return result
-   
-def printBoard(board):
-    for x in range(len(board)):
-        line = ''
-        for y in range(len(board[0])):
-            line += str(board[x][y])
-        print(line)
+    def _generateNeighborIndex(self, point):
+        x, y = point
+        allNeighbors = []
+        allNeighbors.append((x - 1, y - 1))
+        allNeighbors.append((x - 1, y))
+        allNeighbors.append((x - 1, y + 1))
+        allNeighbors.append((x, y + 1))
+        allNeighbors.append((x, y - 1))
+        allNeighbors.append((x + 1, y - 1))
+        allNeighbors.append((x + 1, y))
+        allNeighbors.append((x + 1, y + 1))
+        return [(x, y) for x, y in allNeighbors if x >= 0 and x < len(self.board) and y >= 0 and y < len(self.board[0])]
+    
+    def generateNewBoard(self, board):
+        result = []
+        for x in range(len(board)):
+            result.append([])
+            for y in range(len(board[0])):
+                result[x].append(self._generateNewCell((x, y), board))
+        return result
+       
+    def printBoard(self, board):
+        for x in range(len(board)):
+            line = ''
+            for y in range(len(board[0])):
+                line += str(board[x][y])
+            print(line)
+    
 
     
 class TestNewBoardGeneration(unittest.TestCase):
 
     def test_dead_cell_with_three_neigbors_becomes_alive(self):
-        board = [[0, 1, 0],
-                 [0, 1, 0],
-                 [0, 1, 0]]
-        self.assertEqual(ALIVE, generateNewCell((1,0), board))
-        self.assertEqual(ALIVE, generateNewCell((1,2), board))
+        board = Board([[0, 1, 0],
+                       [0, 1, 0],
+                       [0, 1, 0]])
+        self.assertEqual(Board.ALIVE, board._generateNewCell((1,0), board))
+        self.assertEqual(Board.ALIVE, board._generateNewCell((1,2), board))
         
     def test_dead_cell_with_two_neigbors_remains_dead(self):
-        board = [[0, 1, 0],
-                 [0, 1, 0],
-                 [0, 1, 0]]
-        self.assertEqual(DEAD, generateNewCell((0,0), board))
-        self.assertEqual(DEAD, generateNewCell((2,0), board))
+        board = Board([[0, 1, 0],
+                       [0, 1, 0],
+                       [0, 1, 0]])
+        self.assertEqual(Board.DEAD, board._generateNewCell((0,0), board))
+        self.assertEqual(Board.DEAD, board._generateNewCell((2,0), board))
         
     def test_dead_cell_with_more_three_neighbors_remains_dead(self):
-        board = [[0, 1, 0, 0],
-                 [0, 1, 1, 1],
-                 [0, 1, 0, 1]]
-        self.assertEqual(DEAD, generateNewCell((2,2), board))
+        board = Board([[0, 1, 0, 0],
+                       [0, 1, 1, 1],
+                       [0, 1, 0, 1]])
+        self.assertEqual(Board.DEAD, board._generateNewCell((2,2), board))
         
     def test_alive_cell_with_two_neigbors_remains_alive(self):
-        board = [[0, 1, 0],
-                 [0, 1, 0],
-                 [0, 1, 0]]
-        self.assertEqual(ALIVE, generateNewCell((1,1), board))
+        board = Board([[0, 1, 0],
+                       [0, 1, 0],
+                       [0, 1, 0]])
+        self.assertEqual(Board.ALIVE, board._generateNewCell((1,1), board))
 
     def test_alive_cell_with_three_neigbors_remains_alive(self):
-        board = [[0, 1, 1],
-                 [0, 1, 1],
-                 [0, 0, 0]]
-        self.assertEqual(ALIVE, generateNewCell((1,1), board))
+        board = Board([[0, 1, 1],
+                       [0, 1, 1],
+                       [0, 0, 0]])
+        self.assertEqual(Board.ALIVE, board._generateNewCell((1,1), board))
         
     def test_alive_cell_with_four_neigbors_dies(self):
-        board = [[0, 1, 1],
-                 [0, 1, 1],
-                 [0, 1, 0]]
-        self.assertEqual(DEAD, generateNewCell((1,1), board))
+        board = Board([[0, 1, 1],
+                       [1, 1, 1],
+                       [1, 0, 0]])
+        self.assertEqual(Board.DEAD, board._generateNewCell((1,1), board))
         
 
 
