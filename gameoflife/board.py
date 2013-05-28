d7db9243b1d5b06cc3d3fcfbb6c271424290fe21
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2013-05-28 22:24:27 +0200

    Corrected the evolve method. Removed board parameter from the _evolveCell method.

--- gameoflife/board.py
+++ gameoflife/board.py
@@ -16,7 +16,7 @@ class Board(object):
         x, y = point
         return self.board[x][y]
     
-    def _evolveCell(self, coord, board):
+    def _evolveCell(self, coord):
         cell = self._getCell(coord) 
         aliveNeighbors = reduce(lambda x,y: x + y, map(lambda point: self._getCell(point), self._generateNeighborCoords(coord)))
         
@@ -44,11 +44,12 @@ class Board(object):
                         (x + 1, y + 1)]
         return [(x, y) for x, y in allIndices if x >= 0 and x < len(self.board) and y >= 0 and y < len(self.board[0])]
     
-    def evolve(self, board):
-        return [self._evolveCell((x, y), board)
-                for x in range(len(board))
-                for y in range(len(board[0]))]
-       
+    def evolve(self):
+        return Board([[self._evolveCell((x, y)) 
+                       for y in range(len(self.board[0]))] 
+                      for x in range(len(self.board))])
+    
+
     def __str__(self):
         result = ''
         for x in range(len(self.board)):
@@ -66,40 +67,47 @@ class TestNewBoardGeneration(unittest.TestCase):
         board = Board([[0, 1, 0],
                        [0, 1, 0],
                        [0, 1, 0]])
-        self.assertEqual(Board.ALIVE, board._evolveCell((1,0), board))
-        self.assertEqual(Board.ALIVE, board._evolveCell((1,2), board))
+        self.assertEqual(Board.ALIVE, board._evolveCell((1,0)))
+        self.assertEqual(Board.ALIVE, board._evolveCell((1,2)))
         
     def test_dead_cell_with_two_neigbors_remains_dead(self):
         board = Board([[0, 1, 0],
                        [0, 1, 0],
                        [0, 1, 0]])
-        self.assertEqual(Board.DEAD, board._evolveCell((0,0), board))
-        self.assertEqual(Board.DEAD, board._evolveCell((2,0), board))
+        self.assertEqual(Board.DEAD, board._evolveCell((0,0)))
+        self.assertEqual(Board.DEAD, board._evolveCell((2,0)))
         
     def test_dead_cell_with_more_three_neighbors_remains_dead(self):
         board = Board([[0, 1, 0, 0],
                        [0, 1, 1, 1],
                        [0, 1, 0, 1]])
-        self.assertEqual(Board.DEAD, board._evolveCell((2,2), board))
+        self.assertEqual(Board.DEAD, board._evolveCell((2,2)))
         
     def test_alive_cell_with_two_neigbors_remains_alive(self):
         board = Board([[0, 1, 0],
                        [0, 1, 0],
                        [0, 1, 0]])
-        self.assertEqual(Board.ALIVE, board._evolveCell((1,1), board))
+        self.assertEqual(Board.ALIVE, board._evolveCell((1,1)))
 
     def test_alive_cell_with_three_neigbors_remains_alive(self):
         board = Board([[0, 1, 1],
                        [0, 1, 1],
                        [0, 0, 0]])
-        self.assertEqual(Board.ALIVE, board._evolveCell((1,1), board))
+        self.assertEqual(Board.ALIVE, board._evolveCell((1,1)))
         
     def test_alive_cell_with_four_neigbors_dies(self):
         board = Board([[0, 1, 1],
                        [1, 1, 1],
                        [1, 0, 0]])
-        self.assertEqual(Board.DEAD, board._evolveCell((1,1), board))
-        
+        self.assertEqual(Board.DEAD, board._evolveCell((1,1)))
+
+class TestEvolve(unittest.TestCase):
+    def test_evolve_correctly(self):
+        board = Board([[0, 1, 0],
+                       [0, 1, 0],
+                       [0, 1, 0]])
+        expectedCells = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
+        self.assertEqual(expectedCells, board.evolve().board)
 
 
 #if __name__ == "__main__":
